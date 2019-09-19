d72a1b838b905fa8cc5d3940365ff558d3d9c553
jeremyben <bensimon.jeremy@gmail.com>
2019-09-19 12:50:52 +0200

    chore: prepare express-middlewares package

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,9 @@ const config = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
 	testPathIgnorePatterns: ['/node_modules/', '/__tests__/shared/', '/dist/'],
+	moduleNameMapper: {
+		'^@reflet/(.*)$': join(__dirname, '$1', 'src'),
+	},
 	// https://jestjs.io/docs/en/configuration#setupfiles-array
 	setupFiles: [join(__dirname, 'testing', 'eachfile-setup.ts')],
 	globals: {
