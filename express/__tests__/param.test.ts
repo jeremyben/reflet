e2998e608434f6dacfc683709a8d670f78cbc90c
jeremyben <bensimon.jeremy@gmail.com>
2019-09-21 14:53:23 +0200

    test(express): gather param decorator tests

--- express/__tests__/param.test.ts
+++ express/__tests__/param.test.ts
@@ -1,93 +1,161 @@
 import supertest from 'supertest'
-import express, { Response, Request, json } from 'express'
+import express, { Response, Request, RequestHandler, json, urlencoded } from 'express'
 import {
 	register,
-	Router,
 	Get,
 	Post,
 	Put,
-	Patch,
 	Use,
 	Res,
 	Body,
 	Params,
 	Headers,
-	createParamDecorator,
 	Query,
-	Delete,
+	createParamDecorator,
 } from '../src'
+import { extractMiddlewares } from '../src/middleware-decorator'
+import { extractParamsMiddlewares } from '../src/param-decorators'
+import { getGlobalMiddlewares } from '../src/register'
 import { log } from '../../testing/tools'
 
-const CurrentUser = createParamDecorator((req: Request & { user?: any }) => req.user)
-const BodyTrimmed = (subKey: string) => createParamDecorator((req) => req.body[subKey].trim(), [json()])
+describe('basic decorators', () => {
+	@Use((req, res, next) => {
+		req.headers.via = 'via'
+		req.headers.shared = 'shared'
+		next()
+	})
+	class Controller {
+		@Get()
+		async get(@Headers('via') via: string, @Headers<any>('shared') shared: string, @Res res: Response) {
+			res.send({ via, shared })
+		}
 
-@Use((req: Request & { user?: any }, res, next) => {
-	req.user = { id: 1, name: 'jeremy' }
-	req.headers.via = 'via'
-	req.headers.shared = 'shared'
-	next()
-})
-@Router('/bar')
-class BarRouter {
-	@Get()
-	async get(@Headers('via') via: string, @Headers<any>('shared') shared: string, @Res res: Response) {
-		res.send({ via, shared })
+		@Post()
+		post(@Body body: { foo: number }, @Body<{ foo: number }>('foo') foo: number, @Res res: Response) {
+			res.send({ bar: foo * body.foo })
+		}
+
+		@Put('/:id')
+		put(@Params params: any, @Query('q') q: string, @Res() res: Response) {
+			const bar = Number.parseInt(params.id, 10) * 2
+			res.send({ bar, baz: q })
+		}
 	}
 
-	@Post()
-	post(@Body body: { foo: number }, @Body<{ foo: number }>('foo') foo: number, @Res res: Response) {
-		res.send({ bar: foo * body.foo })
-	}
+	const rq = supertest(register(express(), [Controller]))
 
-	@Put('/:id')
-	put(@Params params: any, @Query('q') q: string, @Res() res: Response) {
-		const bar = Number.parseInt(params.id, 10) * 2
-		res.send({ bar, baz: q })
-	}
+	test('@Headers', async () => {
+		const res = await rq.get('')
+		expect(res.body).toEqual({ via: 'via', shared: 'shared' })
+	})
 
-	@Delete()
-	delete(@CurrentUser user: object, @Res() res: Response) {
-		res.send({ user })
-	}
+	test('@Body with auto body-parser', async () => {
+		const res = await rq.post('').send({ foo: 5 })
+		expect(res.body).toEqual({ bar: 25 })
+	})
 
-	@Patch()
-	patch(@BodyTrimmed('foot') foot: string, @BodyTrimmed('pub') pub: string, @Res() res: Response) {
-		foot = foot + '!'
-		pub = pub + '!'
-		res.send({ foot, pub })
-	}
-}
-
-const rq = supertest(register(express(), [BarRouter]))
-
-test('@Headers', async () => {
-	const res = await rq.get('/bar')
-	expect(res.body).toEqual({ via: 'via', shared: 'shared' })
-})
-
-test('@Body with auto body-parser', async () => {
-	const res = await rq.post('/bar').send({ foo: 5 })
-	expect(res.body).toEqual({ bar: 25 })
-})
-
-test('@Params and @Query', async () => {
-	const res = await rq.put('/bar/25?q=yes')
-	expect(res.body).toEqual({ bar: 50, baz: 'yes' })
+	test('@Params and @Query', async () => {
+		const res = await rq.put('/25?q=yes')
+		expect(res.body).toEqual({ bar: 50, baz: 'yes' })
+	})
 })
 
 describe('custom decorators', () => {
+	const CurrentUser = createParamDecorator((req: Request & { user?: any }) => req.user)
+	const BodyTrimmed = (subKey: string) => createParamDecorator((req) => req.body[subKey].trim(), [json()])
+
+	@Use((req: Request & { user?: any }, res, next) => {
+		req.user = { id: 1, name: 'jeremy' }
+		next()
+	})
+	class Controller {
+		@Get()
+		get(@CurrentUser user: object, @Res() res: Response) {
+			res.send({ user })
+		}
+
+		@Put()
+		put(@BodyTrimmed('foot') foot: string, @BodyTrimmed('pub') pub: string, @Res() res: Response) {
+			foot = foot + '!'
+			pub = pub + '!'
+			res.send({ foot, pub })
+		}
+	}
+
+	const rq = supertest(register(express(), [Controller]))
+
 	test('simple decorator', async () => {
-		const res = await rq.delete('/bar')
+		const res = await rq.get('')
 		expect(res.body).toEqual({
 			user: { id: 1, name: 'jeremy' },
 		})
 	})
 
 	test('decorator with options and middleware', async () => {
-		const res = await rq.patch('/bar').send({ foot: ' Champions du monde  ', pub: '  O Ballec  ' })
+		const res = await rq.put('').send({ foot: ' Champions du monde  ', pub: '  O Ballec  ' })
 		expect(res.body).toEqual({
 			foot: 'Champions du monde!',
 			pub: 'O Ballec!',
 		})
+	})
+})
+
+describe('param middlewares deduplication', () => {
+	test('body-parsers', async () => {
+		const BodyTrimmed = (subKey: string) =>
+			createParamDecorator((req) => req.body[subKey].trim(), [json()], true)
+
+		@Use(json(), urlencoded({ extended: false }))
+		class FooRouter {
+			@Use(json())
+			@Post()
+			post(
+				@BodyTrimmed('foo') fooTrimmed: string,
+				@Body('foo') foo: string,
+				@Body body: any,
+				@Res res: Response
+			) {
+				res.send(fooTrimmed)
+			}
+		}
+
+		const sharedMwares = extractMiddlewares(FooRouter)
+		const routeMwares = extractMiddlewares(FooRouter, 'post')
+		const paramMwares = extractParamsMiddlewares(FooRouter, 'post', [[], sharedMwares, routeMwares])
+
+		expect(paramMwares).toHaveLength(0)
+	})
+
+	test('custom middleware', async () => {
+		type User = { id: number; name: string }
+		type RequestAuth = Request & { user?: User }
+
+		const authent: RequestHandler = (req: RequestAuth, res, next) => {
+			req.user = { id: 1, name: 'jeremy' }
+			next()
+		}
+
+		const CurrentUser = createParamDecorator((req: RequestAuth) => req.user!, [authent], true)
+
+		@Use(authent)
+		class BarRouter {
+			@Post('user/:yo')
+			post(@CurrentUser user: User, @Res res: Response) {
+				res.send(user)
+			}
+		}
+		const app = express()
+		app.use(authent)
+		const globalMwares = getGlobalMiddlewares(app)
+		const sharedMwares = extractMiddlewares(BarRouter)
+		const routeMwares = extractMiddlewares(BarRouter, 'post')
+		const paramMwares = extractParamsMiddlewares(BarRouter, 'post', [
+			globalMwares,
+			sharedMwares,
+			routeMwares,
+		])
+
+		expect(sharedMwares.some((m) => globalMwares.includes(m))).toBe(true)
+		expect(paramMwares).toHaveLength(0)
 	})
 })
