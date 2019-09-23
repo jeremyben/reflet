855b7f7d2977db611e0a60f5e0936502f0839ba9
jeremyben <bensimon.jeremy@gmail.com>
2019-09-23 15:57:21 +0200

    feat(express-middlewares): apply middewares condtionally with if decorator

---
+++ express-middlewares/src/if-decorator.ts
@@ -0,0 +1,36 @@
+import { Use } from '@reflet/express'
+import { Request, RequestHandler } from 'express'
+import { isPromise } from './type-guards'
+
+/**
+ * Applies middlewares conditionally.
+ *
+ * @param condition - function that should return a boolean (asynchronously or not). Has access to the Request object.
+ * @param middlewares - middlewares that are applied in case the condition returns `true`.
+ *
+ * @remarks
+ * Example :
+ *
+ * ```ts
+ * ＠UseIf((req) => req.method === 'POST', [express.json()])
+ * class Foo {}
+ * ```
+ *
+ * @public
+ */
+export function UseIf(
+	condition: (req: Request) => boolean | Promise<boolean>,
+	middlewares: RequestHandler[]
+) {
+	return Use((req, res, next) => {
+		const ok = condition(req)
+
+		if (isPromise(ok)) ok.then((yes) => apply(yes))
+		else apply(ok)
+
+		function apply(yes: boolean) {
+			if (yes) for (const middleware of middlewares) middleware(req, res, next)
+			else next()
+		}
+	})
+}
--- express-middlewares/src/index.ts
+++ express-middlewares/src/index.ts
@@ -5,3 +5,5 @@ export { UseInterceptor } from './interceptor-decorator'
 export { UseOnFinish } from './onfinish-decorator'
 
 export { UseSet, UseHeader, UseType, UseContentType } from './response-headers-decorators'
+
+export { UseIf } from './if-decorator'
