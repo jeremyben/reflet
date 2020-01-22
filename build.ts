470c244ddf005eb3ee3f21a0cd4a473ece30fcb2
jeremyben <bensimon.jeremy@gmail.com>
2020-01-22 22:44:34 +0100

    chore: replace api-extractor with tsc-prog bundling option

--- build.ts
+++ build.ts
@@ -1,117 +1,55 @@
 import { join } from 'path'
 import { build } from 'tsc-prog'
 import ts from 'typescript'
-import * as ae from '@microsoft/api-extractor'
 import * as tsdoc from '@microsoft/tsdoc'
 import { writeFileSync } from 'fs'
 
 const basePath = process.cwd()
-const rootDir = 'src'
-const outDir = 'dist'
-const tmpDir = 'temp'
+const dtsEntryPoint = 'index.d.ts'
 
 build({
 	basePath,
 	configFilePath: 'tsconfig.json',
 	compilerOptions: {
-		rootDir,
-		outDir,
-		// Emit declaration files in a temp folder so api-extractor can bundle them in the next step
+		rootDir: 'src',
+		outDir: 'dist',
 		declaration: true,
-		declarationDir: tmpDir,
-		declarationMap: true,
 		skipLibCheck: true,
+		stripInternal: true,
 	},
-	include: [`${rootDir}/**/*`],
+	include: [`src/**/*`],
 	exclude: ['**/__tests__', '**/test.ts', '**/*.test.ts', '**/*.spec.ts', 'node_modules'],
-	clean: { outDir: true, declarationDir: true },
+	clean: { outDir: true },
+	bundleDeclaration: { entryPoint: dtsEntryPoint },
 })
 
-const dtsPath = join(basePath, 'dist', 'index.d.ts')
-const state = bundleDefinitions('public')
-syncOverloadsDoc(state.program.getCompilerOptions() as ts.CompilerOptions)
+syncOverloadsDoc()
 
-/**
- * @see https://api-extractor.com/pages/overview/demo_rollup/
- */
-function bundleDefinitions(release: '' | 'beta' | 'public' = '') {
-	const logLevel = {
-		none: { logLevel: 'none' as ae.ExtractorLogLevel },
-		warning: { logLevel: 'warning' as ae.ExtractorLogLevel },
-		error: { logLevel: 'error' as ae.ExtractorLogLevel },
-	}
-
-	// https://api-extractor.com/pages/commands/config_file/
-	const configObject: ae.IConfigFile = {
-		projectFolder: basePath,
-		mainEntryPointFilePath: '<projectFolder>/temp/index.d.ts',
-		compiler: { tsconfigFilePath: '<projectFolder>/tsconfig.json' },
-		dtsRollup: {
-			enabled: true,
-			untrimmedFilePath: release === '' ? dtsPath : '',
-			betaTrimmedFilePath: release === 'beta' ? dtsPath : '',
-			publicTrimmedFilePath: release === 'public' ? dtsPath : '',
-			omitTrimmingComments: false,
-		},
-		messages: {
-			compilerMessageReporting: {
-				default: logLevel.error,
-			},
-			extractorMessageReporting: {
-				default: logLevel.error,
-				'ae-forgotten-export': logLevel.warning,
-				'ae-unresolved-inheritdoc-reference': logLevel.none,
-				'ae-internal-missing-underscore': logLevel.none,
-			},
-			tsdocMessageReporting: {
-				default: logLevel.warning,
-				'tsdoc-undefined-tag': logLevel.none,
-				'tsdoc-unsupported-tag': logLevel.none,
-			},
-		},
-	}
-
-	const config = ae.ExtractorConfig.prepare({
-		configObject,
-		configObjectFullPath: undefined,
-		packageJsonFullPath: join(basePath, 'package.json'),
-	})
-
-	const res = ae.Extractor.invoke(config, { localBuild: true, showVerboseMessages: true })
-
-	if (res.succeeded) {
-		console.log(`API Extractor succeeded with ${res.warningCount} warnings`)
-	} else {
-		console.error(`API Extractor failed with ${res.errorCount} errors and ${res.warningCount} warnings`)
-		process.exit(1)
-	}
-
-	return res.compilerState
-}
-
-function syncOverloadsDoc(options: ts.CompilerOptions) {
+function syncOverloadsDoc() {
 	console.log('Syncing inherited docs for overloaded functions')
 
-	const docConfig = new tsdoc.TSDocConfiguration()
-	docConfig.setSupportForTags(tsdoc.StandardTags.allDefinitions, true)
+	const tsdocConfig = new tsdoc.TSDocConfiguration()
+	tsdocConfig.setSupportForTags(tsdoc.StandardTags.allDefinitions, true)
 
-	const customTagDefinitions = [
-		new tsdoc.TSDocTagDefinition({
-			tagName: '@decorator',
-			syntaxKind: tsdoc.TSDocTagSyntaxKind.ModifierTag,
-		}),
-		new tsdoc.TSDocTagDefinition({
-			tagName: '@see',
-			syntaxKind: tsdoc.TSDocTagSyntaxKind.BlockTag,
-			allowMultiple: true,
-		}),
-	]
-	docConfig.addTagDefinitions(customTagDefinitions, true)
+	tsdocConfig.addTagDefinitions(
+		[
+			new tsdoc.TSDocTagDefinition({
+				tagName: '@see',
+				syntaxKind: tsdoc.TSDocTagSyntaxKind.BlockTag,
+				allowMultiple: true,
+			}),
+			new tsdoc.TSDocTagDefinition({
+				tagName: '@decorator',
+				syntaxKind: tsdoc.TSDocTagSyntaxKind.ModifierTag,
+			}),
+		],
+		true
+	)
 
-	const docParser = new tsdoc.TSDocParser(docConfig)
+	const docParser = new tsdoc.TSDocParser(tsdocConfig)
 
-	const program = ts.createProgram({ options, rootNames: [dtsPath] })
-	const dtsFile = program.getSourceFile(dtsPath)!
+	const dtsPath = join(basePath, 'dist', dtsEntryPoint)
+	const dtsFile = ts.createProgram({ options: {}, rootNames: [dtsPath] }).getSourceFile(dtsPath)!
 	const dtsText = dtsFile.getFullText()
 	const docMap = new Map<string, string>()
 
