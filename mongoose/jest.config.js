fb38b1cbc0929c2536ad0a63c851f61fb7ce592e
jeremyben <bensimon.jeremy@gmail.com>
2023-07-28 13:55:23 +0200

    tests: udpate jest config

--- mongoose/jest.config.js
+++ mongoose/jest.config.js
@@ -1,26 +1,19 @@
 const { join } = require('path')
 
-// https://kulshekhar.github.io/ts-jest/user/config/#advanced
-const tsjestPreset = require('ts-jest/presets').defaults
-
 /** @type {import('@jest/types').Config.InitialOptionsWithRootDir} */
 const config = {
 	rootDir: process.cwd(),
-	// https://github.com/shelfio/jest-mongodb
-	preset: '@shelf/jest-mongodb',
-	transform: { ...tsjestPreset.transform },
+	preset: '@shelf/jest-mongodb', // https://github.com/shelfio/jest-mongodb
 	testPathIgnorePatterns: ['/node_modules/', '/__tests__/shared/', '/dist/'],
 	moduleNameMapper: {
 		'^@reflet/(.*)$': join(__dirname, '..', '$1', 'src'),
 	},
-	// https://jestjs.io/docs/en/configuration#setupfiles-array
 	setupFiles: [join(__dirname, '..', 'testing', 'eachfile-setup.ts')],
 	setupFilesAfterEnv: [join(__dirname, 'jest-setup-afterenv.ts')],
 	coverageReporters: ['json-summary', 'text', 'lcov'],
-	globals: {
-		'ts-jest': {
-			diagnostics: { warnOnly: true },
-		},
+	transform: {
+		// https://kulshekhar.github.io/ts-jest/docs/getting-started/options
+		'^.+\\.tsx?$': ['ts-jest', { diagnostics: { warnOnly: true } }],
 	},
 }
 
