fdb7e22ad0a7ab79c52ef170c84b1e86024116fc
jeremyben <bensimon.jeremy@gmail.com>
2023-07-30 01:21:37 +0200

    chore: fix mongoose build

--- build.ts
+++ build.ts
@@ -1,5 +1,5 @@
 import { basename, join } from 'path'
-import { build } from 'tsc-prog'
+import { build, EmitOptions } from 'tsc-prog'
 import * as ts from 'typescript'
 import * as tsdoc from '@microsoft/tsdoc'
 import { writeFileSync } from 'fs'
@@ -7,6 +7,23 @@ import { writeFileSync } from 'fs'
 const basePath = process.cwd()
 const folderName = basename(basePath)
 const dtsEntryPoint = 'index.d.ts'
+
+let bundle: EmitOptions.Bundle | undefined
+
+if (folderName !== 'http' && folderName !== 'mongoose') {
+	bundle = {
+		entryPoint: dtsEntryPoint,
+		augmentations: false,
+	}
+}
+
+if (folderName === 'mongoose') {
+	bundle = {
+		entryPoint: dtsEntryPoint,
+		augmentations: false,
+		extras: [{ position: 'after-imports', declaration: 'import * as mongoose from "mongoose";' }],
+	}
+}
 
 build({
 	basePath,
@@ -21,7 +38,7 @@ build({
 	include: [`src/**/*`],
 	exclude: ['**/__tests__', '**/test.ts', '**/*.test.ts', '**/*.spec.ts', 'node_modules'],
 	clean: { outDir: true },
-	bundleDeclaration: folderName !== 'http' ? { entryPoint: dtsEntryPoint, augmentations: false } : undefined,
+	bundleDeclaration: bundle,
 })
 
 syncOverloadsDoc()
